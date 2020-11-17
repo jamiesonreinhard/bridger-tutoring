b328ca05c20652c96282bf49ef7b76039dfb571c
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-11-17 14:39:47 -0600

    created rails api with devise user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+
+  enum role: { student: 0, tutor: 1, admin: 2 }
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
