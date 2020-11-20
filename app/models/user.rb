b80cd9dfe78be52fde5c28abcd5b0d523aa1eec5
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-11-20 11:54:37 -0600

    removed devise and added jwt, can now login and create users with post requests in postman

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,12 @@
 class User < ApplicationRecord
-  acts_as_token_authenticatable
+  has_secure_password
 
-  enum role: { student: 0, tutor: 1, admin: 2 }
-  has_one :student, dependent: :destroy
-  has_one :tutor, dependent: :destroy
+  enum role: [:student, :tutor, :admin]
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  validates :email, presence: true
-  validates :email, uniqueness: true
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  after_initialize do
+    if self.new_record?
+      self.role ||= :standard
+    end
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users, only: [:create]
+  post "/login", to: "users#login"
+  get "/auto_login", to: "users#auto_login"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :students, only: [ :index, :show, :update, :create ]
