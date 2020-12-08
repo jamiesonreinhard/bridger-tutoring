c0ce221b4c34ecae1e8b8c3336ceb2f358b9dc43
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-12-07 17:11:04 -0700

    trying whatever

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token, avatar: @user.avatar}
+      render json: {user: @user, token: token}
     else
       render json: {error: "Invalid email or password!"}
     end
