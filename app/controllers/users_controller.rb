19c98409459873a6ae44d12a9f0fb93df76650e3
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2020-12-09 07:21:41 -0700

    removed avatar json from users create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     if @user.valid?
       
       token = encode_token({user_id: @user.id})
-      render json: {user: @user, token: token, avatar: avatar}
+      render json: {user: @user, token: token}
     else
       render json: {error: "Invalid email or password!"}
     end
