afbce3aec0a3c45de54305a22e75986c931b2cbe
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-01-03 19:21:46 -0700

    tutor has many taggings

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,6 +1,7 @@
 class Tutor < ApplicationRecord
   belongs_to :user
   has_many :appointments, dependent: :destroy
+  has_many :taggings
   has_one_attached :avatar
   acts_as_taggable_on :subjects
 end
